02054af16fc6f8cef12558f7f9ad7c604ddd3530
Wandile Simamane <st10445696@vcconnect.edu.za>
2026-04-27 02:07:24 +0200

    feat: add deleteAccount to AuthRepository interface

--- app/src/main/java/com/example/flipfinance/domain/repository/AuthRepository.kt
+++ app/src/main/java/com/example/flipfinance/domain/repository/AuthRepository.kt
@@ -9,8 +9,8 @@ interface AuthRepository {
     suspend fun login(email: String, pass: String): Result<Unit>
     suspend fun register(email: String, pass: String): Result<Unit>
     fun logout()
+    suspend fun deleteAccount(): Result<Unit>
 
     // Reset User Password
     suspend fun sendPasswordResetEmail(email: String): Result<Unit>
-
 }
