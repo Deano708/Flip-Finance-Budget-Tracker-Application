4370a511e895bf32147aeb8d10b5f2f69aaf35c3
Quen-Drew-02 <166541589+Quen-Drew-02@users.noreply.github.com>
2026-04-29 12:23:11 +0200

    style: Redesigned SettingsScreen with modular components and Shape.kt alignment

---
+++ app/src/main/java/com/example/flipfinance/ui/components/settings/settingsTextButton.kt
@@ -0,0 +1,53 @@
+package com.example.flipfinance.ui.components.settings
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+
+/*
+   Title: Rows, Columns & Basic Sizing - Android Jetpack Compose - Part 2
+   Author: Phillip Lackner
+   Date: 5 years ago
+   Date accessed: 29/04/2026
+   Code version : 1
+   Availability: https://youtu.be/rHKeRWK3zL4?si=BIcdBEid7DIozjYu
+*/
+
+/*
+   Title: Modifiers - Android Jetpack Compose - Part 3
+   Author: Phillip Lackner
+   Date: 5 years ago
+   Date accessed: 29/04/2026
+   Code version : 1
+   Availability: https://youtu.be/XCuC_p3E0qo?si=e-mzwWJ2Dx5MDG5W
+*/
+
+/*
+   Title: Textfields, Buttons & Showing Snackbars - Android Jetpack Compose - Part 7
+   Author: Phillip Lackner
+   Date: 5 years ago
+   Date accessed: 29/04/2026
+   Code version : 1
+   Availability: https://youtu.be/_yON9d9if6g?si=SzA1f3U4XmFhxOUw
+*/
+
+@Composable
+fun SettingsTextButton(text: String, onClick: () -> Unit) {
+    TextButton(
+        onClick = onClick,
+        modifier = Modifier.fillMaxWidth(),
+        contentPadding = PaddingValues(vertical = 12.dp, horizontal = 4.dp),
+        shape = MaterialTheme.shapes.small // Using Shape.kt Small
+    ) {
+        Row(modifier = Modifier.fillMaxWidth(), horizontalArrangement = Arrangement.Start) {
+            Text(text, color = MaterialTheme.colorScheme.onSurface)
+        }
+    }
+}
